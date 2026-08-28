53ed64bc995d39b37b8433bc8702d0210175d29a
ayush <ayushmane852@gmail.com>
2026-08-28 16:25:20 +0530

    Update Doc Head to Documentation Head so department modal displays Documentation TEAM

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -164,7 +164,7 @@ export default function Team() {
     },
     {
       id: 'doc_head',
-      role: 'Doc Head',
+      role: 'Documentation Head',
       category: 'domain',
       image: '/Images/Team/Doc head.jpeg',
       tag: 'DOCUMENTATION HEAD',
