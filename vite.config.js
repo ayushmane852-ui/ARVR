42380c472fe525b11345875675e0b7e9e269c26a
rsmp15 <riteshmahatme18@gmail.com>
2026-09-14 01:58:35 +0530

    chore: configure https and host in vite for mobile wifi testing

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), basicSsl()],
+  server: {
+    host: true,
+    port: 5173,
+  },
 })
 
